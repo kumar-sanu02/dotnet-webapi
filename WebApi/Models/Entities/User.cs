5aeca68955e0c2d9b615f6904e16a0ea300237d3
kumar.sanu02 <kumar.sanu02@infosys.com>
2025-01-28 11:06:52 +0530

    user Entity is added

---
+++ WebApi/Models/Entities/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models.Entities
+{
+    public class User
+    {
+        public Employee Employees { get; set; }
+        [ForeignKey("Employee")]
+        public int Id{get;set;}
+        [Required][MaxLength(50)]
+        public int Address { get; set; }
+    }
+}
