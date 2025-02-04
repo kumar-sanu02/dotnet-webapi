4b2d035ac3685c03d152c436ef2e092dd1ea205a
kumar.sanu02 <kumar.sanu02@infosys.com>
2025-02-04 19:24:42 +0530

    comments added

--- WebApi/Models/Entities/Employee.cs
+++ WebApi/Models/Entities/Employee.cs
@@ -8,6 +8,7 @@ namespace WebApi.Models.Entities
     public class Employee
     {
         public int Id { get; set; }
+        //minimum length of name should 3 but less than 50
        [Required][MinLength(3)][MaxLength(50)]
         public  string Name { get; set; }
         [Required]
